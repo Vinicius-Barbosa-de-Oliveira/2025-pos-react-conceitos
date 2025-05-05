9e278073262acbf81a98ddf3b1b6cbae61f3f6c0
Vinicius Barbosa <94053437+Vinicius-Barbosa-de-Oliveira@users.noreply.github.com>
2025-05-05 09:38:02 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,13 @@ const Titulo = () => <h1>React - Conceitos básicos</h1>;
 
 const SubTitulo = () => (<h2>Lista de tarefas</h2>);
 
+const tarefa = () => (<h3>Projeto PIU</h3>);
+
 const Cabecalho = () => (
   <>
     <Titulo />
     <SubTitulo />
+    <tarefa />
   </>
 );
 
